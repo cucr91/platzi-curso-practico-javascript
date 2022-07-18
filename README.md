df13ce1162a6e5a3ec3cf2e1d5df57e8174724d4
Camilo <camilocalderonrojas@gmail.com>
2022-07-17 20:56:25 -0400

    Creacion funcion descuento y porcentaje

--- README.md
+++ README.md
@@ -9,3 +9,10 @@
  -  tercer paso: crear funciones
  - cuarto paso: Integrar javascrip en Html
  
+## Taller Nª 2  Porcentajes y descuentos
+
+ -  Primer paso: Definir formulas
+ -  Segundo paso: Implementar las formulas en Javascrip
+ -  tercer paso: crear funciones
+ - cuarto paso: Integrar javascrip en Html
+  
---
+++ descuentos.js
@@ -0,0 +1,54 @@
+//const precioOriginal = 120;
+// const descuento = 18;
+
+/* console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento
+});
+
+
+function calcularPrecioConDescuento(precio, descuento){
+
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.Value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.Value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("resultPrice");
+
+    resultPrice.innerText = "El precio con descuento es $" + precioConDescuento;
+}
+*/
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
+    return precioConDescuento;
+  }
+  
+  function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
+  
