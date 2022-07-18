8df5a7441eef9a5e6cd62e9c84be7d01bcd7ed04
Camilo <camilocalderonrojas@gmail.com>
2022-07-18 17:22:21 -0400

    actualizacion con descuentos y cupones

--- descuentos.js
+++ descuentos.js
@@ -31,56 +31,58 @@ function onClickButtonPriceDiscount(){
     resultPrice.innerText = "El precio con descuento es $" + precioConDescuento;
 }
 */
-
+  
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-  
     return precioConDescuento;
   }
   
- /* function onClickButtonPriceDiscount() {
+  
+  function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
     
-    const inputCoupon = document.getElementById("InputCoupon");
-    const couponValue = inputCoupon.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
   
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
   
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-  }*/
-  
+  }
+
   const coupons = [
     "JuanDC_es_Batman",
     "pero_no_le_digas_a_nadie",
     "es_un_secreto",
-];
-function onClickButtonPriceDiscount() {
-    const inputPrice = document.getElementById("InputPrice");
+    ];
+
+function onClickButtonPriceDiscountCuopon(){
+    const inputPrice = document.getElementById("InputPriceCoupon");
     const priceValue = inputPrice.value;
     
     const inputCoupon = document.getElementById("InputCoupon");
     const couponValue = inputCoupon.value;
   
-    let descuento;
-  
-    switch(couponValue) {
-      case coupons[0]: // "JuanDC_es_Batman"
-        descuento = 15;
-      break;
-      case coupons[1]: // "pero_no_le_digas_a_nadie"
-        descuento = 30;
-      break;
-      case coupons[2]: // "es_un_secreto"
-        descuento = 25;
-      break;
+
+    let descuento;  
+        switch(couponValue) {
+        case coupons[0]: // "JuanDC_es_Batman"
+          descuento = 15;
+        break;
+        case coupons[1]: // "pero_no_le_digas_a_nadie"
+          descuento = 30;
+        break;
+        case coupons[2]: // "es_un_secreto"
+          descuento = 25;
+        break;
     }
-  
-  
     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
-  
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+  //const  resultPC = document.getElementById("ResultPC");
+  // resultPC.innerText = "El precio con cupon de descuento es de: $" + precioConDescuento;
+  alert("Felicidades, tu cupon de descuento da el precio de: $" + precioConDescuento);
+  console.log(descuento)
+  console.log(priceValue)
   }
