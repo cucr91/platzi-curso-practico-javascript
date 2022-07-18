2a36367157a11f0d283513f78bea7a7e08e6aad4
Camilo <camilocalderonrojas@gmail.com>
2022-07-18 15:54:11 -0400

    Implementacion cupon de descuento con array y switch

--- descuentos.js
+++ descuentos.js
@@ -39,16 +39,48 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
   }
   
-  function onClickButtonPriceDiscount() {
+ /* function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
     
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
   
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
   
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-  }
+  }*/
   
+  const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    let descuento;
+  
+    switch(couponValue) {
+      case coupons[0]: // "JuanDC_es_Batman"
+        descuento = 15;
+      break;
+      case coupons[1]: // "pero_no_le_digas_a_nadie"
+        descuento = 30;
+      break;
+      case coupons[2]: // "es_un_secreto"
+        descuento = 25;
+      break;
+    }
+  
+  
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
